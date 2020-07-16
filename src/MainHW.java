290274cb355e11a37aa9a68d4796ea6324b5f939
FF <5kla@mail.ru>
2020-07-17 01:00:47 +0500

    hw 1

--- src/MainHW.java
+++ src/MainHW.java
@@ -1,5 +1,9 @@
 public class MainHW {
     public static void main(String[] args) {
-        
+        System.out.println("hello world!");
+    }
+
+    public static int mlti(int a, int b) {
+        return a * b;
     }
 }
